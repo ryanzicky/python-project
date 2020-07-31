e79facb7a2627a3370c1ba9afa9a37abf1422b68
zhourui <zhourui@tuhu.cn>
2020-07-31 18:13:36 +0800

    init

--- scrapy/vmgirls/Main.py
+++ scrapy/vmgirls/Main.py
@@ -59,11 +59,38 @@ def get_dic_list():
 
 
 def get_img_list():
-    dic_list = get_dic_list()
-    # for dic in dic_list:
-    #     print(dic)
-    print(requests.get(dic_list[2], headers=headers, proxies=proxies))
+    try:
+        dic_list = get_dic_list()
+        # for dic in dic_list:
+        #     print(dic)
+        dic_url = dic_list[2]
+        html = requests.get(dic_url, headers=headers, proxies=proxies)
+        response = etree.HTML(html.text)
+        div_list = response.xpath('//*[@id="js-repo-pjax-container"]/div[2]/div/div[3]/div[3]/div/div')
+
+        img_list = []
+        for div in div_list:
+            href = div.xpath('./div[2]/span/a/@href')
+            title = div.xpath('./div[2]/span/a/text()')
+            if title:
+                img_list.append(base_url + href[0])
+
+        return img_list
+    except Exception as e:
+        print('获取图片路径失败!, ', e)
+
+    return []
+
+
+def get_img():
+    img_list = get_img_list()
+    # print(img_list)
+    for img_url in img_list:
+        html = requests.get(img_url, headers=headers, proxies=proxies)
+        response = etree.HTML(html.text)
+        src = response.xpath('/html/body/div[4]/div/main/div[2]/div/div[3]/div[2]/div/span/img/@src')
+        print(base_url + src[0])
 
 
 if __name__ == '__main__':
-    get_img_list()
+    get_img()
